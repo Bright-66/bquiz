c388cb4598ccb11300802b12fe57d07b21ec6b84
sunbody1024 <sunbody1024@gmail.com>
2024-12-30 11:10:52 +0800

    建立每日瀏灠統計功能

--- web0902/admin.php
+++ web0902/admin.php
@@ -1,4 +1,6 @@
-﻿<!DOCTYPE html
+﻿<?php include_once "api/db.php";?>
+
+<!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -20,34 +22,36 @@
     </div>
     <div id="all">
         <div id="title">
-            <?=date("m 月 d 號 l");?> | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
+            <?=date("m 月 d 號 l");?> | 今日瀏覽: <?=$Total->find(['date'=>date("Y-m-d")])['total'];?>
+            | 累積瀏覽: <?=$Total->sum('total');?>
+            <a href="index.php" style="float:right">回首頁</a>
 
-        <div id="title2">
-            <a href="index.php" title="健康促進網-回首">
-                <img src="icon/02B01.jpg" alt="健康促進網-回首頁">
-            </a>
-        </div>
-        <div id="mm">
-            <div class="hal" id="lef">
-                <a class="blo" href="?do=acc">帳號管理</a>
-                <a class="blo" href="?do=po">分類網誌</a>
-                <a class="blo" href="?do=news">最新文章管理</a>
-                <a class="blo" href="?do=know">講座管理</a>
-                <a class="blo" href="?do=que">問卷管理</a>
+            <div id="title2">
+                <a href="index.php" title="健康促進網-回首">
+                    <img src="icon/02B01.jpg" alt="健康促進網-回首頁">
+                </a>
             </div>
-            <div class="hal" id="main">
-                <div>
-                    <div style="width:75% display:inline-block;">
-                        <marquee>請民眾踴躍投稿電子報，讓電子報成為大家相互交流分享的園地!詳見最新文章
-                        </marquee>
-                    </div>
+            <div id="mm">
+                <div class="hal" id="lef">
+                    <a class="blo" href="?do=acc">帳號管理</a>
+                    <a class="blo" href="?do=po">分類網誌</a>
+                    <a class="blo" href="?do=news">最新文章管理</a>
+                    <a class="blo" href="?do=know">講座管理</a>
+                    <a class="blo" href="?do=que">問卷管理</a>
                 </div>
-                <span style="width:23; display:inline-block;">
-                    <a href="?do=login">會員登入</a>
-                </span>
+                <div class="hal" id="main">
+                    <div>
+                        <div style="width:75% display:inline-block;">
+                            <marquee>請民眾踴躍投稿電子報，讓電子報成為大家相互交流分享的園地!詳見最新文章
+                            </marquee>
+                        </div>
+                    </div>
+                    <span style="width:23; display:inline-block;">
+                        <a href="?do=login">會員登入</a>
+                    </span>
 
-            </div>
-            <?php
+                </div>
+                <?php
 				$do=$_GET['do']??'main';
 				$file="back/".$do.".php";
 				if(file_exists($file)){
@@ -58,13 +62,13 @@
 				}
 			?>
 
+            </div>
         </div>
-    </div>
-    <div id="bottom">
-        本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2024健康促進網社群平台 All Right Reserved
-        <br>
-        服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
-    </div>
+        <div id="bottom">
+            本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2024健康促進網社群平台 All Right Reserved
+            <br>
+            服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
+        </div>
     </div>
 
 </body>
