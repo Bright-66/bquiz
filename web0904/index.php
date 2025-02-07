79b8d49143d6a885547e775a32de4be074e02e42
sunbody1024 <sunbody1024@gmail.com>
2025-02-07 11:53:26 +0800

    完成購物車流程,最新消息,跑馬燈,及css微調

--- web0904/index.php
+++ web0904/index.php
@@ -6,13 +6,14 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-    <title>精品電子商務網站」</title>
+    <title>┌精品電子商務網站」</title>
     <link href="./css/css.css" rel="stylesheet" type="text/css">
+    <script src="./js/jquery-3.4.1.min.js"></script>
     <script src="./js/js.js"></script>
 </head>
 
 <body>
-    <iframe name="back" style="display:none;"></iframe>
+
     <div id="main">
         <div id="top">
             <a href="?">
@@ -27,10 +28,13 @@
                 <a href="?do=admin">管理登入</a>
             </div>
 
-            情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~
+
         </div>
+
         <div id="left" class="ct">
             <div style="min-height:400px;">
+
+
             </div>
             <span>
                 <div>進站總人數</div>
@@ -39,15 +43,17 @@
             </span>
         </div>
         <div id="right">
-            <?php
-$do=$_GET['do']??'main';
-$file="front/".$do.".php";
-if(file_exists($file)){
-    include $file;
-}else{
-    include "front/admin.php";
-}
-?>
+            <?php 
+        $do=$_GET['do']??'main';
+        $file="front/".$do.".php";
+        if(file_exists($file)){
+            include $file;
+        }else{
+            include "front/main.php";
+        }
+
+
+        ?>
         </div>
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
             頁尾版權 : </div>
