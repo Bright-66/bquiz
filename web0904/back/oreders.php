c5fbb1454d766de53dde691b63918174d44e66d0
sunbody1024 <sunbody1024@gmail.com>
2025-02-20 10:54:36 +0800

    訂單管理功能

---
+++ web0904/back/oreders.php
@@ -0,0 +1,30 @@
+<h2 class="ct">訂單管理</h2>
+<table class="all">
+    <tr class="tt ct">
+        <td>訂單編號</td>
+        <td>金額</td>
+        <td>會員帳號</td>
+        <td>姓名</td>
+        <td>下單時間</td>
+        <td>操作</td>
+
+    </tr>
+    <?php
+
+        $rows = $Order->all();
+        foreach ($rows as $row):
+    ?>
+    <tr class="pp ct">
+        <td><?php echo $row['no']; ?></td>
+        <td><?php echo $row['total']; ?></td>
+        <td><?php echo $row['acc']; ?></td>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row("Y/m/d", strtotime($row['order_time'])); ?></td>
+        </td>
+        <button onclick="del('Order',<?php echo $row['id']; ?>)">刪除</button>
+        </td>
+    </tr>
+    <?php
+        endforeach;
+    ?>
+</table>
